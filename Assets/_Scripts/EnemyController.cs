65fd251e77c98a59fe4f5dd6d46835f577b38143
Charlie Campbell <chacha9669@hotmail.com>
2025-03-05 21:20:58 +1100

    98% performance increase
    removed basemonobehaviour

--- Assets/_Scripts/EnemyController.cs
+++ Assets/_Scripts/EnemyController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : BaseMonoBehaviour
+public class EnemyController : MonoBehaviour
 {
     [Tooltip("Movement Speed of Enemy")]
     [SerializeField] private float moveSpeed = 10f;
--- Assets/_Scripts/ProjectileHandler.cs
+++ Assets/_Scripts/ProjectileHandler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileHandler : BaseMonoBehaviour
+public class ProjectileHandler : MonoBehaviour
 {
     [SerializeField] private float projectileSpeed;
     // Start is called before the first frame update
