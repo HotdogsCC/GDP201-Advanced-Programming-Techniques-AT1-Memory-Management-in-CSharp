b17769271aa716d836070c84fad9d5c5f99a5f3a
Charlie Campbell <chacha9669@hotmail.com>
2025-03-16 16:21:46 +1100

    0 bytes of garbage

--- Assets/_Scripts/GUIHandler.cs
+++ Assets/_Scripts/GUIHandler.cs
@@ -1,5 +1,6 @@
 
 using System.Collections;
+using System.Text;
 using UnityEngine;
 
 public class GUIHandler : MonoBehaviour
@@ -10,27 +11,41 @@ public class GUIHandler : MonoBehaviour
     [SerializeField] private TMPro.TextMeshProUGUI healthText;
     [SerializeField] private TMPro.TextMeshProUGUI timeScale;
 
+    private EnemyManager manager;
+    private StringBuilder stringBuilder = new StringBuilder();
+    
     // Start is called before the first frame update
     void Start()
     {
+        manager = FindObjectOfType<EnemyManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Update the bullets fired text
         var player = GameObject.FindGameObjectWithTag("Player");
+        
+        //Update the bullets fired text
         int bulletsFired = player.GetComponent<TurretShoot>().GetNumberOfShellsFired();
-        bulletsFiredText.text = "Bullets fired: " + bulletsFired;
+
+        stringBuilder.Append("Bullet fired: ");
+        stringBuilder.Append(bulletsFired);
+        bulletsFiredText.text = stringBuilder.ToString();
+        stringBuilder.Clear();
         
         //Update the health text
         int health = player.GetComponent<PlayerHealth>().GetHealth();
-        healthText.text = "Health: " + health;
+        stringBuilder.Append("Health: ");
+        stringBuilder.Append(health);
+        healthText.text = stringBuilder.ToString();
+        stringBuilder.Clear();
         
         //Update the enemies killed text
-        var manager = FindObjectOfType<EnemyManager>();
         int enemiesKilled = manager.EnemiesKilled();
-        enemiesKilledText.text = "Enemies Killed: " + enemiesKilled;
+        stringBuilder.Append("Enemies Killed: ");
+        stringBuilder.Append(enemiesKilled);
+        enemiesKilledText.text = stringBuilder.ToString();
+        stringBuilder.Clear();
 
         timeScale.text = string.Format("Time Scale: {0:0.00}", Time.timeScale);
     }
